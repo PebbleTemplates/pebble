85448c588751019aa40c4cdd7ea0fed8f13fe25c
Mitchell <Mitchell@mitchell-pc>
2012-12-26 20:36:05 -0700

    Added license information

--- src/main/java/com/mitchellbosecke/pebble/PebbleEngine.java
+++ src/main/java/com/mitchellbosecke/pebble/PebbleEngine.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * This file is part of Pebble.
+ * 
+ * Copyright (c) 2012 Mitchell Bosecke.
+ * 
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
+ * Unported License. To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ ******************************************************************************/
 package com.mitchellbosecke.pebble;
 
 import java.io.UnsupportedEncodingException;
@@ -27,10 +36,6 @@ import com.mitchellbosecke.pebble.tokenParser.TokenParserBroker;
 import com.mitchellbosecke.pebble.tokenParser.TokenParserBrokerImpl;
 import com.mitchellbosecke.pebble.utils.Operator;
 
-/**
- * @author Mitchell Bosecke <mitchellbosecke@gmail.com>
- * 
- */
 public class PebbleEngine {
 
 	/*
--- src/main/java/com/mitchellbosecke/pebble/node/NodeRoot.java
+++ src/main/java/com/mitchellbosecke/pebble/node/NodeRoot.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * This file is part of Pebble.
+ * 
+ * Copyright (c) 2012 Mitchell Bosecke.
+ * 
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 
+ * Unported License. To view a copy of this license, visit 
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ ******************************************************************************/
 package com.mitchellbosecke.pebble.node;
 
 import java.util.Map;
