6b6c3bb7c07103ce14d6a6aa2d0872ab4aa4b3f0
Mitchell Bosecke <mitchellbosecke@gmail.com>
2014-02-07 18:55:06 -0700

    Reduced unnecessary complexity of the class attribute caching

--- src/main/java/com/mitchellbosecke/pebble/template/ClassAttributeCacheEntry.java
+++ src/main/java/com/mitchellbosecke/pebble/template/ClassAttributeCacheEntry.java
@@ -6,47 +6,18 @@ import java.util.Map;
 
 public class ClassAttributeCacheEntry {
 
-	private class AttributeWithHitCount {
-		private final Member member;
-		private int hitCount = 0;
-
-		public AttributeWithHitCount(Member member) {
-			this.member = member;
-		}
-
-		public Member getMember() {
-			return member;
-		}
-
-		public int getHitCount() {
-			return hitCount;
-		}
-
-		public void incrementHitCount() {
-			this.hitCount++;
-		}
-	}
-
-	private final Map<String, AttributeWithHitCount> attributes = new HashMap<>();
+	private final Map<String, Member> attributes = new HashMap<>();
 
 	public boolean hasAttribute(String attributeName) {
 		return attributes.containsKey(attributeName);
 	}
 
 	public Member getAttribute(String attributeName) {
-		AttributeWithHitCount attr = attributes.get(attributeName);
-		attr.incrementHitCount();
-		return attr.getMember();
-	}
-
-	public int getAttributeHitCount(String attributeName) {
-		AttributeWithHitCount attr = attributes.get(attributeName);
-		return attr.getHitCount();
+		return attributes.get(attributeName);
 	}
 
 	public void putAttribute(String attributeName, Member attribute) {
-		AttributeWithHitCount attr = new AttributeWithHitCount(attribute);
-		attributes.put(attributeName, attr);
+		attributes.put(attributeName, attribute);
 	}
 
 }
