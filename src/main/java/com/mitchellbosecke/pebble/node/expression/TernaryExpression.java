fb033857272bb8f60dd9bcb3b670c1ace3f20189
Mitchell Bosecke <mitchellbosecke@gmail.com>
2014-07-10 18:29:57 -0600

    Fixed null pointer exception occurring when the first expression in a ternary expression returned null.

--- src/main/java/com/mitchellbosecke/pebble/node/expression/TernaryExpression.java
+++ src/main/java/com/mitchellbosecke/pebble/node/expression/TernaryExpression.java
@@ -27,7 +27,7 @@ public class TernaryExpression implements Expression<Object> {
 
 	@Override
 	public Object evaluate(PebbleTemplateImpl self, EvaluationContext context) throws PebbleException {
-		if (expression1.evaluate(self, context)) {
+		if (expression1.evaluate(self, context) == true) {
 			return expression2.evaluate(self, context);
 		} else {
 			return expression3.evaluate(self, context);
