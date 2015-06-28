c40333da542b302b5a390a20414cd5074561f409
Mitchell Bösecke <mitchellbosecke@gmail.com>
2015-06-28 11:43:20 -0600

    Removed unused import from ServletLoader

--- src/main/java/com/mitchellbosecke/pebble/loader/ServletLoader.java
+++ src/main/java/com/mitchellbosecke/pebble/loader/ServletLoader.java
@@ -1,7 +1,6 @@
 package com.mitchellbosecke.pebble.loader;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
