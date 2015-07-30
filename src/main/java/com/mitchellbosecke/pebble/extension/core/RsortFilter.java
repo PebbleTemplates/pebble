d252d6e360afb76a64046e1cff5a74494be6339c
Barakat <b4r4k47@hotmail.com>
2015-07-30 12:26:57 +0300

    Add "rsort" filter to sort list items in the reverse order.

---
+++ src/main/java/com/mitchellbosecke/pebble/extension/core/RsortFilter.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * This file is part of Pebble.
+ * 
+ * Copyright (c) 2014 by Mitchell Bösecke
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ ******************************************************************************/
+package com.mitchellbosecke.pebble.extension.core;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import com.mitchellbosecke.pebble.extension.Filter;
+
+/**
+ * Sort list items in the reverse order
+ * 
+ * @author Barakat Soror
+ *
+ */
+public class RsortFilter implements Filter {
+
+    @Override
+    public List<String> getArgumentNames() {
+        return null;
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @Override
+    public List<Comparable> apply(Object input, Map<String, Object> args) {
+        if (input == null) {
+            return null;
+        }
+        List<Comparable> collection = (List<Comparable>) input;
+        Collections.sort(collection, Collections.reverseOrder());
+        return collection;
+    }
+
+}
