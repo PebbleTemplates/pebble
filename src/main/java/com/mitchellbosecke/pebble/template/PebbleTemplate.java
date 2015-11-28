7c4dd29b0d17bab4534c2f64a649a885375d82b5
Mitchell Bösecke <mitchellbosecke@gmail.com>
2015-11-28 16:52:25 -0700

    Added javadocs to PebbleTemplate

--- src/main/java/com/mitchellbosecke/pebble/template/PebbleTemplate.java
+++ src/main/java/com/mitchellbosecke/pebble/template/PebbleTemplate.java
@@ -1,28 +1,60 @@
 /*******************************************************************************
  * This file is part of Pebble.
- *
+ * <p>
  * Copyright (c) 2014 by Mitchell Bösecke
- *
+ * <p>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  ******************************************************************************/
 package com.mitchellbosecke.pebble.template;
+
+import com.mitchellbosecke.pebble.error.PebbleException;
 
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Locale;
 import java.util.Map;
 
-import com.mitchellbosecke.pebble.error.PebbleException;
-
 public interface PebbleTemplate {
 
+    /**
+     * Evaluate the template.
+     *
+     * @param writer    The results of the evaluation are written to this writer.
+     * @throws PebbleException
+     * @throws IOException
+     */
     void evaluate(Writer writer) throws PebbleException, IOException;
 
+    /**
+     * Evaluate the template.
+     *
+     * @param writer    The results of the evaluation are written to this writer.
+     * @param locale    The locale used during the evaluation of the template.
+     * @throws PebbleException
+     * @throws IOException
+     */
     void evaluate(Writer writer, Locale locale) throws PebbleException, IOException;
 
-    void evaluate(Writer writer, Map<String, Object> map) throws PebbleException, IOException;
+    /**
+     * Evaluate the template.
+     *
+     * @param writer    The results of the evaluation are written to this writer.
+     * @param context   The variables used during the evaluation of the template.
+     * @throws PebbleException
+     * @throws IOException
+     */
+    void evaluate(Writer writer, Map<String, Object> context) throws PebbleException, IOException;
 
-    void evaluate(Writer writer, Map<String, Object> map, Locale locale) throws PebbleException, IOException;
+    /**
+     * Evaluate the template.
+     *
+     * @param writer    The results of the evaluation are written to this writer.
+     * @param context   The variables used during the evaluation of the template.
+     * @param locale    The locale used during the evaluation of the template.
+     * @throws PebbleException
+     * @throws IOException
+     */
+    void evaluate(Writer writer, Map<String, Object> context, Locale locale) throws PebbleException, IOException;
 
 }
