3eef6cd1ea6f1daf5468bd0600c65fcd8b36c93e
hectorlf <hectorlopezfernandez@hotmail.com>
2016-03-02 11:18:18 +0100

    Apply formatting

--- src/main/java/com/mitchellbosecke/pebble/extension/core/MergeFilter.java
+++ src/main/java/com/mitchellbosecke/pebble/extension/core/MergeFilter.java
@@ -33,43 +33,44 @@ public class MergeFilter implements Filter {
 
     @Override
     public Object apply(Object input, Map<String, Object> args) {
-    	Object items = args.get("items");
+        Object items = args.get("items");
         if (input == null && items == null) {
             throw new IllegalArgumentException("The two arguments to be merged are null");
         } else if (input != null && items == null) {
-        	return input;
+            return input;
         } else if (items != null && input == null) {
-        	return items;
+            return items;
         }
         // left hand side argument defines resulting type
         if (input instanceof Map) {
-        	return mergeAsMap((Map<?,?>)input, items);
+            return mergeAsMap((Map<?, ?>) input, items);
         } else if (input instanceof List) {
-        	return mergeAsList((List<?>)input, items);
+            return mergeAsList((List<?>) input, items);
         } else if (input.getClass().isArray()) {
-        	return mergeAsArray(input, items);
+            return mergeAsArray(input, items);
         } else {
-        	throw new IllegalArgumentException("The object being filtered is not a Map/List/Array");
+            throw new IllegalArgumentException("The object being filtered is not a Map/List/Array");
         }
     }
 
-    private Object mergeAsMap(Map<?,?> arg1, Object arg2) {
-    	Map<?,?> collection1 = arg1;
-    	Map<Object,Object> output = null;
+    private Object mergeAsMap(Map<?, ?> arg1, Object arg2) {
+        Map<?, ?> collection1 = arg1;
+        Map<Object, Object> output = null;
         if (arg2 instanceof Map) {
-        	Map<?,?> collection2 = (Map<?,?>)arg2;
-        	output = new HashMap<Object,Object>(collection1.size() + collection2.size() + 16);
-        	output.putAll(collection1);
-        	output.putAll(collection2);
+            Map<?, ?> collection2 = (Map<?, ?>) arg2;
+            output = new HashMap<Object, Object>(collection1.size() + collection2.size() + 16);
+            output.putAll(collection1);
+            output.putAll(collection2);
         } else if (arg2 instanceof List) {
-            List<?> collection2 = (List<?>)arg2;
-        	output = new HashMap<Object,Object>(collection1.size() + collection2.size() + 16);
-        	output.putAll(collection1);
-        	for (Object o : collection2) {
-        	    output.put(o,o);
-        	}
+            List<?> collection2 = (List<?>) arg2;
+            output = new HashMap<Object, Object>(collection1.size() + collection2.size() + 16);
+            output.putAll(collection1);
+            for (Object o : collection2) {
+                output.put(o, o);
+            }
         } else {
-        	throw new UnsupportedOperationException("Currently, only Maps and Lists can be merged with a Map. Arg2: " + arg2.getClass().getName());
+            throw new UnsupportedOperationException(
+                    "Currently, only Maps and Lists can be merged with a Map. Arg2: " + arg2.getClass().getName());
         }
         return output;
     }
@@ -78,17 +79,18 @@ public class MergeFilter implements Filter {
         List<?> collection1 = arg1;
         List<Object> output = null;
         if (arg2 instanceof Map) {
-        	Map<?,?> collection2 = (Map<?,?>)arg2;
-        	output = new ArrayList<Object>(collection1.size() + collection2.size() + 16);
-        	output.addAll(collection1);
-        	output.addAll(collection2.entrySet());
+            Map<?, ?> collection2 = (Map<?, ?>) arg2;
+            output = new ArrayList<Object>(collection1.size() + collection2.size() + 16);
+            output.addAll(collection1);
+            output.addAll(collection2.entrySet());
         } else if (arg2 instanceof List) {
-            List<?> collection2 = (List<?>)arg2;
-        	output = new ArrayList<Object>(collection1.size() + collection2.size() + 16);
-        	output.addAll(collection1);
-        	output.addAll(collection2);
+            List<?> collection2 = (List<?>) arg2;
+            output = new ArrayList<Object>(collection1.size() + collection2.size() + 16);
+            output.addAll(collection1);
+            output.addAll(collection2);
         } else {
-        	throw new UnsupportedOperationException("Currently, only Maps and Lists can be merged with a List. Arg2: " + arg2.getClass().getName());
+            throw new UnsupportedOperationException(
+                    "Currently, only Maps and Lists can be merged with a List. Arg2: " + arg2.getClass().getName());
         }
         return output;
     }
@@ -97,7 +99,9 @@ public class MergeFilter implements Filter {
         Class<?> arg1Class = arg1.getClass().getComponentType();
         Class<?> arg2Class = arg2.getClass().getComponentType();
         if (!arg1Class.equals(arg2Class)) {
-            throw new UnsupportedOperationException("Currently, only Arrays of the same component class can be merged. Arg1: " + arg1Class.getName() + ", Arg2: " + arg2Class.getName());
+            throw new UnsupportedOperationException(
+                    "Currently, only Arrays of the same component class can be merged. Arg1: " + arg1Class.getName()
+                            + ", Arg2: " + arg2Class.getName());
         }
         Object output = Array.newInstance(arg1Class, Array.getLength(arg1) + Array.getLength(arg2));
         System.arraycopy(arg1, 0, output, 0, Array.getLength(arg1));
