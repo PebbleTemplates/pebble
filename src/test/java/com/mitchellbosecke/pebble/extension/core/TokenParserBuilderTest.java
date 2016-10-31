22b01561a7aed0b42a13b0f3232f987ee3da44e8
menostos <menostos@gmail.com>
2016-10-31 11:13:18 +0100

    removed unused imports

--- src/test/java/com/mitchellbosecke/pebble/extension/core/TokenParserBuilderTest.java
+++ src/test/java/com/mitchellbosecke/pebble/extension/core/TokenParserBuilderTest.java
@@ -1,19 +1,11 @@
 package com.mitchellbosecke.pebble.extension.core;
 
-import com.mitchellbosecke.pebble.error.ParserException;
-import com.mitchellbosecke.pebble.lexer.Token;
-import com.mitchellbosecke.pebble.node.RenderableNode;
-import com.mitchellbosecke.pebble.parser.Parser;
-import com.mitchellbosecke.pebble.tokenParser.AbstractTokenParser;
-import com.mitchellbosecke.pebble.tokenParser.BlockTokenParser;
-import com.mitchellbosecke.pebble.tokenParser.ForTokenParser;
 import com.mitchellbosecke.pebble.tokenParser.TokenParser;
-import com.sun.org.apache.xml.internal.security.utils.I18n;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class TokenParserBuilderTest {
 
