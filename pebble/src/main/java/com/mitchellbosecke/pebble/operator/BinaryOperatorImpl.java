cb9c85f48337f94aa2853ca32014307a5ac7f3ff
Victor.Lalykin <victor.lalykin@BILLING.RU>
2020-02-28 20:43:27 +0300

    Allow completely control the instantiation of the binary expression class

--- pebble/src/main/java/com/mitchellbosecke/pebble/operator/BinaryOperatorImpl.java
+++ pebble/src/main/java/com/mitchellbosecke/pebble/operator/BinaryOperatorImpl.java
@@ -8,7 +8,12 @@
  */
 package com.mitchellbosecke.pebble.operator;
 
+import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.node.expression.BinaryExpression;
+import com.mitchellbosecke.pebble.node.expression.FilterExpression;
+import com.mitchellbosecke.pebble.node.expression.NegativeTestExpression;
+import com.mitchellbosecke.pebble.node.expression.PositiveTestExpression;
+import java.util.function.Supplier;
 
 public class BinaryOperatorImpl implements BinaryOperator {
 
@@ -16,16 +21,38 @@ public class BinaryOperatorImpl implements BinaryOperator {
 
   private final String symbol;
 
-  private final Class<? extends BinaryExpression<?>> nodeClass;
+  private final Supplier<? extends BinaryExpression<?>> nodeSupplier;
+
+  private final BinaryOperatorType type;
 
   private final Associativity associativity;
 
+  /**
+   * This constuctor left for backward compatibility with custom extensions
+   */
   public BinaryOperatorImpl(String symbol, int precedence,
       Class<? extends BinaryExpression<?>> nodeClass,
       Associativity associativity) {
+    this(symbol, precedence, () -> {
+      try {
+        return nodeClass.newInstance();
+      } catch (InstantiationException | IllegalAccessException e) {
+        throw new PebbleException(e, "Error instantiating class " + nodeClass.getName());
+      }
+    }, getDefaultType(nodeClass), associativity);
+  }
+
+  /**
+   * This constuctor allows you to completely control the instantiation of the expression class
+   */
+  public BinaryOperatorImpl(String symbol, int precedence,
+      Supplier<? extends BinaryExpression<?>> nodeSupplier,
+      BinaryOperatorType type,
+      Associativity associativity) {
     this.symbol = symbol;
     this.precedence = precedence;
-    this.nodeClass = nodeClass;
+    this.nodeSupplier = nodeSupplier;
+    this.type = type;
     this.associativity = associativity;
   }
 
@@ -40,12 +67,28 @@ public class BinaryOperatorImpl implements BinaryOperator {
   }
 
   @Override
-  public Class<? extends BinaryExpression<?>> getNodeClass() {
-    return this.nodeClass;
+  public BinaryExpression<?> getInstance() {
+    return this.nodeSupplier.get();
+  }
+
+  @Override
+  public BinaryOperatorType getType() {
+    return this.type;
   }
 
   @Override
   public Associativity getAssociativity() {
     return this.associativity;
+  }
+
+  private static BinaryOperatorType getDefaultType(Class<? extends BinaryExpression<?>> nodeClass) {
+    if (FilterExpression.class.equals(nodeClass)) {
+      return BinaryOperatorType.FILTER;
+    } else if (PositiveTestExpression.class.equals(nodeClass) || NegativeTestExpression.class
+        .equals(nodeClass)) {
+      return BinaryOperatorType.TEST;
+    } else {
+      return BinaryOperatorType.NORMAL;
+    }
   }
 }
