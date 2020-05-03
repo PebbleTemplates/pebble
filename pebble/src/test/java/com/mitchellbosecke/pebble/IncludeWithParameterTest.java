09cad2afbe3fda7f42d0b10d6dfee9cd8d8acff8
Eric Bussieres <erbussieres@gmail.com>
2020-05-03 15:16:56 -0400

    #506: Add unit test

--- pebble/src/test/java/com/mitchellbosecke/pebble/IncludeWithParameterTest.java
+++ pebble/src/test/java/com/mitchellbosecke/pebble/IncludeWithParameterTest.java
@@ -1,17 +1,15 @@
 package com.mitchellbosecke.pebble;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
-
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  * This class tests if includes with parameters work.
@@ -59,4 +57,23 @@ class IncludeWithParameterTest {
 
   }
 
+  @Test
+  void testIncludeWithParameterObject() throws PebbleException, IOException {
+
+    PebbleEngine pebble = new PebbleEngine.Builder().strictVariables(false).build();
+    PebbleTemplate template = pebble
+        .getTemplate("templates/template.includeWithParameterObject1.peb");
+    Map<String, Object> context = new HashMap<>();
+    context.put("object", new TestObject());
+
+    Writer writer = new StringWriter();
+    template.evaluate(writer, context);
+
+    assertEquals("Hello title", writer.toString());
+  }
+
+  public static class TestObject {
+
+    public String title = "title";
+  }
 }
