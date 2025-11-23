725a06894923303f89de1c4a9098e5b465599247
Eric Bussieres <erbussieres@gmail.com>
2025-11-23 08:44:01 -0500

    chore: remove use of getLiteralTemplate in unit tests

--- pebble/src/test/java/io/pebbletemplates/pebble/extension/ExtensionCustomizerTest.java
+++ pebble/src/test/java/io/pebbletemplates/pebble/extension/ExtensionCustomizerTest.java
@@ -3,10 +3,10 @@ package io.pebbletemplates.pebble.extension;
 import io.pebbletemplates.pebble.PebbleEngine;
 import io.pebbletemplates.pebble.error.PebbleException;
 import io.pebbletemplates.pebble.extension.core.DisallowExtensionCustomizerBuilder;
+import io.pebbletemplates.pebble.loader.StringLoader;
 import io.pebbletemplates.pebble.template.PebbleTemplate;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.Collections;
 import java.util.HashMap;
@@ -19,14 +19,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ExtensionCustomizerTest {
 
   @Test
-  void upperFilterCannotBeUsed() throws IOException {
+  void upperFilterCannotBeUsed() {
     PebbleEngine pebble = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(RemoveUpperCustomizer::new)
             .build();
 
     Map<String, Object> obj = new HashMap<>();
     obj.put("test", "abc");
-    PebbleTemplate template = pebble.getLiteralTemplate("{{ test | upper }}");
+    PebbleTemplate template = pebble.getTemplate("{{ test | upper }}");
 
     PebbleException exception = assertThrows(PebbleException.class, () -> template.evaluate(new StringWriter(), obj));
     assertTrue(exception.getMessage().contains("upper"),
@@ -36,13 +37,14 @@ class ExtensionCustomizerTest {
   @Test
   void setDisallowedTokenParserTags() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedTokenParserTags(Collections.singletonList("flush"))
                     .build())
             .build();
 
     PebbleException exception = assertThrows(PebbleException.class,
-            () -> pebbleEngine.getLiteralTemplate("{{ k1 }}\n" +
+            () -> pebbleEngine.getTemplate("{{ k1 }}\n" +
                     "{% flush %}\n" +
                     "{{ k2 }}"));
     assertTrue(exception.getMessage().contains("Unexpected tag name \"flush\""));
@@ -51,6 +53,7 @@ class ExtensionCustomizerTest {
   @Test
   void setDisallowedFilters() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedFilterKeys(Collections.singletonList("upper"))
                     .build())
@@ -58,7 +61,7 @@ class ExtensionCustomizerTest {
 
     Map<String, Object> obj = new HashMap<>();
     obj.put("test", "abc");
-    PebbleTemplate template = pebbleEngine.getLiteralTemplate("{{ test | upper }}");
+    PebbleTemplate template = pebbleEngine.getTemplate("{{ test | upper }}");
 
     PebbleException exception = assertThrows(PebbleException.class, () -> template.evaluate(new StringWriter(), obj));
     assertTrue(exception.getMessage().contains("upper"),
@@ -68,6 +71,7 @@ class ExtensionCustomizerTest {
   @Test
   void setDisallowedFunctions() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedFunctionKeys(Collections.singletonList("max"))
                     .build())
@@ -75,7 +79,7 @@ class ExtensionCustomizerTest {
 
     Map<String, Object> obj = new HashMap<>();
     obj.put("age", 30);
-    PebbleTemplate template = pebbleEngine.getLiteralTemplate("{{ max(age, 80) }}");
+    PebbleTemplate template = pebbleEngine.getTemplate("{{ max(age, 80) }}");
 
     PebbleException exception = assertThrows(PebbleException.class, () -> template.evaluate(new StringWriter(), obj));
     assertTrue(exception.getMessage().contains("Function or Macro [max] does not exist"));
@@ -84,38 +88,41 @@ class ExtensionCustomizerTest {
   @Test
   void setDisallowedBinaryOperatorSymbols() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedBinaryOperatorSymbols(Collections.singletonList(">"))
                     .build())
             .build();
 
-    PebbleException exception = assertThrows(PebbleException.class, () -> pebbleEngine.getLiteralTemplate("{% if 10 > 9 %}\n" +
+    PebbleException exception = assertThrows(PebbleException.class, () -> pebbleEngine.getTemplate("{% if 10 > 9 %}\n" +
             "{{ name }}" +
             "{% endif %}"));
     assertTrue(exception.getMessage().contains("Unexpected character [>]"));
   }
 
   @Test
-  void setDisallowedUnaryOperatorSymbols() throws IOException {
+  void setDisallowedUnaryOperatorSymbols() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedUnaryOperatorSymbols(Collections.singletonList("-"))
                     .build())
             .build();
 
-    PebbleException exception = assertThrows(PebbleException.class, () -> pebbleEngine.getLiteralTemplate("{{ -num }}"));
+    PebbleException exception = assertThrows(PebbleException.class, () -> pebbleEngine.getTemplate("{{ -num }}"));
     assertTrue(exception.getMessage().contains("Unexpected token \"OPERATOR\" of value \"-\""));
   }
 
   @Test
-  void setDisallowedTestKeys() throws IOException {
+  void setDisallowedTestKeys() {
     PebbleEngine pebbleEngine = new PebbleEngine.Builder()
+            .loader(new StringLoader())
             .registerExtensionCustomizer(new DisallowExtensionCustomizerBuilder()
                     .disallowedTestKeys(Collections.singletonList("null"))
                     .build())
             .build();
 
-    PebbleTemplate template = pebbleEngine.getLiteralTemplate("{% if 123 is null %}\n" +
+    PebbleTemplate template = pebbleEngine.getTemplate("{% if 123 is null %}\n" +
             "{{ name }}" +
             "{% endif %}");
 
