c75018989b78ea7ad7c1a6641a1cad08b7328628
Eric Bussieres <erbussieres@gmail.com>
2025-11-27 07:45:43 -0500

    chore: fix unit test to run on Windows

--- pebble/src/test/java/io/pebbletemplates/pebble/FileLoaderTest.java
+++ pebble/src/test/java/io/pebbletemplates/pebble/FileLoaderTest.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -34,8 +36,9 @@ public class FileLoaderTest {
   }
 
   @Test
-  void testFileLoader() throws PebbleException, IOException {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath());
+  void testFileLoader() throws PebbleException, IOException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    Loader<?> loader = new FileLoader(prefix);
     loader.setSuffix(".suffix");
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     PebbleTemplate template1 = engine.getTemplate("template.loaderTest.peb");
@@ -45,23 +48,26 @@ public class FileLoaderTest {
   }
 
   @Test
-  void testFileLoaderAbsoluteTemplateName() throws PebbleException {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath());
+  void testFileLoaderAbsoluteTemplateName() throws PebbleException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    Loader<?> loader = new FileLoader(prefix);
     loader.setSuffix(".suffix");
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     assertThrows(LoaderException.class, () -> engine.getTemplate("/template.loaderTest.peb"));
   }
 
   @Test
-  void testFileLoaderTemplateNameIsADirectory() throws PebbleException {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath());
+  void testFileLoaderTemplateNameIsADirectory() throws PebbleException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    Loader<?> loader = new FileLoader(prefix);
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     assertThrows(LoaderException.class, () -> engine.getTemplate("loader"));
   }
 
   @Test
-  void testFileLoaderRelativeTemplateName() throws PebbleException, IOException {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath().replaceFirst("/templates", "/"));
+  void testFileLoaderRelativeTemplateName() throws PebbleException, IOException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).getParent().toString();
+    Loader<?> loader = new FileLoader(prefix);
     loader.setSuffix(".suffix");
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     PebbleTemplate template1 = engine.getTemplate("templates/template.loaderTest.peb");
@@ -71,8 +77,9 @@ public class FileLoaderTest {
   }
 
   @Test
-  void testFileLoaderPathTraversal() throws PebbleException {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath());
+  void testFileLoaderPathTraversal() throws PebbleException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    Loader<?> loader = new FileLoader(prefix);
     loader.setSuffix(".peb");
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     assertThrows(LoaderException.class, () -> engine.getTemplate("../template-tests/DoubleNestedIfStatement"));
@@ -80,8 +87,9 @@ public class FileLoaderTest {
 
   @ParameterizedTest
   @ValueSource(strings = {"%2e%2e%2f", "%2e%2e/", "..%2f", "%2e%2e%5c", "%2e%2e\\", "..%5c", "%252e%252e%255c", "..%255c"})
-  void testFileLoaderPathTraversalEncoded(String relativePath) {
-    Loader<?> loader = new FileLoader(this.getClass().getClassLoader().getResource("templates").getPath());
+  void testFileLoaderPathTraversalEncoded(String relativePath) throws URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    Loader<?> loader = new FileLoader(prefix);
     loader.setSuffix(".peb");
     PebbleEngine engine = new PebbleEngine.Builder().loader(loader).strictVariables(false).build();
     assertThrows(LoaderException.class, () -> engine.getTemplate(relativePath + "template-tests/DoubleNestedIfStatement"));
@@ -91,9 +99,9 @@ public class FileLoaderTest {
    * Tests if relative includes work. Issue #162.
    */
   @Test
-  void testFileLoaderPathWithBackslash() throws IOException {
-    String path = this.getClass().getClassLoader().getResource("templates").getPath();
-    PebbleEngine pebble = new PebbleEngine.Builder().loader(new FileLoader(path)).build();
+  void testFileLoaderPathWithBackslash() throws IOException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    PebbleEngine pebble = new PebbleEngine.Builder().loader(new FileLoader(prefix)).build();
     PebbleTemplate template = pebble.getTemplate("relativepath/subdirectory1/template.forwardslashes.peb".replace("/", "\\")); // ensure backslashes in all environments
     Writer writer = new StringWriter();
     template.evaluate(writer);
@@ -104,9 +112,9 @@ public class FileLoaderTest {
    * Issue #162.
    */
   @Test
-  void testFileLoaderPathWithForwardSlash() throws IOException {
-    String path = this.getClass().getClassLoader().getResource("templates").getPath();
-    PebbleEngine pebble = new PebbleEngine.Builder().loader(new FileLoader(path)).build();
+  void testFileLoaderPathWithForwardSlash() throws IOException, URISyntaxException {
+    String prefix = Paths.get(this.getClass().getClassLoader().getResource("templates").toURI()).toString();
+    PebbleEngine pebble = new PebbleEngine.Builder().loader(new FileLoader(prefix)).build();
     PebbleTemplate template = pebble.getTemplate("relativepath/subdirectory1/template.backwardslashes.peb");
     Writer writer = new StringWriter();
     template.evaluate(writer);
